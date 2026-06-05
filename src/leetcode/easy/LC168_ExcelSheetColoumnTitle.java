beaceedf54b63d10e917f90f40273d246c362e5a
Nakulvasudev T <nakul.thanigai@gmail.com>
2026-06-05 23:55:08 +0530

    LC168_ExcelSheetColoumnTitle

---
+++ src/leetcode/easy/LC168_ExcelSheetColoumnTitle.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+public class LC168_ExcelSheetColoumnTitle {
+    static class Solution {
+        public String convertToTitle(int columnNumber) {
+            StringBuilder sb = new StringBuilder();
+            while(columnNumber>0){
+                columnNumber--;
+                sb.append((char)(columnNumber%26+'A'));
+                columnNumber=columnNumber/26;
+            }
+            return sb.reverse().toString();
+        }
+    }
+
+    static void main() {
+        Solution obj = new Solution();
+        System.out.println("Answer: "+obj.convertToTitle(78));
+    }
+}
