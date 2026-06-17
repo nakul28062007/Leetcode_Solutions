a4df53e6364e3a81a775e7e79e1dd428a02d8d34
Nakulvasudev T <nakul.thanigai@gmail.com>
2026-06-17 12:05:06 +0530

    LC171_ExcelColumnTitle

---
+++ src/leetcode/easy/LC171_ExcelColumnTitle.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+public class LC171_ExcelColumnTitle {
+    static class Solution {
+        public int titleToNumber(String columnTitle) {
+            int i =0;
+            int l= columnTitle.length();
+            double mul=0;
+            int ans=0;
+            String reverse = new StringBuilder(columnTitle).reverse().toString();
+            while(i<l){
+                mul = Math.pow(26,i);
+                ans+= ((((int)reverse.charAt(i))-65)+1)*mul;
+                i++;
+            }
+            return ans;
+        }
+    }
+
+    static void main() {
+        Solution obj = new Solution();
+        System.out.println("Answer: "+ obj.titleToNumber("FXSHRXW"));
+    }
+}
